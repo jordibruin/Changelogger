be943b99a7b9065d91419d16cb02f860a2d60638
Jordi Bruin <jordi@goodsnooze.com>
2024-09-29 20:21:04 +0200

    Initial Commit

---
+++ Releaser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Releaser
+//
+//  Created by Jordi Bruin on 29/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Releaser/ReleaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReleaserApp.swift
+//  Releaser
+//
+//  Created by Jordi Bruin on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReleaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
